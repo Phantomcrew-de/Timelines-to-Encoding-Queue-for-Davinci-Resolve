4440bdec7a5becfe90a14b25cbe8bfd2e877cf61
Phantomcrew_DE <66791116+Phantomcrew-de@users.noreply.github.com>
2022-11-09 12:37:03 +0100

    Add files via upload

---
+++ search_in_timelines_and_encode_non_studio.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#Runs inside Davinci Resolve -> Workspace -> Console -> Python 3
+#Modify the variable in nextline ("v05") to a string you want to search for. Then Copy and paste all text to console.
+
+searchintimelinenamesfor = "_v05"
+
+project_manager = resolve.GetProjectManager()
+project = project_manager.GetCurrentProject()
+print (project.GetName())
+print ("Adding Timelines to Queue:")
+n = project.GetTimelineCount()
+s = 0
+i = 1
+timelineindex = 1
+time.sleep(0.1)
+project.DeleteAllRenderJobs()
+while i <= n:
+    time.sleep(0.1)
+    timelineindex = project.GetTimelineByIndex(i)
+    project.SetCurrentTimeline(timelineindex)
+    timeline = project.GetCurrentTimeline()
+    if timeline.GetName() != None and searchintimelinenamesfor in timeline.GetName():
+        time.sleep(0.1)
+        project.SetRenderSettings({
+		    "SelectAllFrames": True,
+		    "FormatWidth": 1920,
+		    "FormatHeight": 1080,
+        })
+        pid = project.AddRenderJob()
+        print ("Add: Timeline " + str(i) + " of " + str(n) + " -> " + timeline.GetName())
+        time.sleep(0.2)
+        i = i + 1
+    else:
+        print("Skip: Timeline " + str(i) + " of " + str(n) + " -> " + timeline.GetName())
+        i = i + 1    
+print("All Timelines Added")
+project.StartRendering(isInteractiveMode = True)
+print("Rendering in Progress...")
+       
+    
