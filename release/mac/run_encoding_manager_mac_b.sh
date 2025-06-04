ac21a99440756eb058b8c902d41da954d986ada1
Phantomcrew_DE <66791116+Phantomcrew-de@users.noreply.github.com>
2025-06-04 23:14:27 +0200

    Add files via upload

---
+++ release/mac/run_encoding_manager_mac_b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Verzeichnis des Skripts ermitteln
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Resolve-Scripting-Modulpfad setzen
+export PYTHONPATH="/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules:$PYTHONPATH"
+
+# Python-Skript relativ zum Skriptverzeichnis ausführen
+python3 "$SCRIPT_DIR/search_in_timelines_and_encode_GUI_mac_b.py"
